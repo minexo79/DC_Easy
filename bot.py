a1ebfcc2f0a4145055c42c85ebf260b59c962824
ShibaInu <60269668+neo123440@users.noreply.github.com>
2020-02-13 07:17:23 +0800

    add command avatar

--- bot.py
+++ bot.py
@@ -7,5 +7,13 @@ bot = commands.Bot(commands_prefix="dcb.")
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name="use dcb.help."))
     print("[DCB] Bot is on!")
+    
+@commands.command() #截取對方的頭貼
+async def avatar(ctx, id: int):
+    user = await client.fetch_user(id)
+    avatar_url = user.avatar_url
+    embed=discord.Embed(color=0xff66fd)
+    embed.set_image(url=avatar_url)
+    await ctx.send(embed=embed)
 
 bot.run("YOUR TOKEN")
