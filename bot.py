c0bab69133001cd0f08b9ccc591260c000ac1204
ShibaInu <60269668+neo123440@users.noreply.github.com>
2020-02-13 07:20:50 +0800

    debug

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="use dcb.help."))
     print("[DCB] Bot is on!")
     
-@commands.command() #截取對方的頭貼
+@bot.command() #截取對方的頭貼
 async def avatar(ctx, id: int):
     user = await client.fetch_user(id)
     avatar_url = user.avatar_url
