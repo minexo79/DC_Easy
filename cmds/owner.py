4e826ec9b8663d9cc6d007b37a7b080978297cd9
XOT <54303621+minexo79@users.noreply.github.com>
2020-02-18 21:15:37 +0800

    add two functions "load & unload extension"

--- cmds/owner.py
+++ cmds/owner.py
@@ -4,17 +4,31 @@ from core.classes import Cog_Extension
 
 class owner(Cog_Extension):
 
-    @commands.command() #重新裝載Cog
+    @commands.command()
     @commands.is_owner() 
-    async def re(self, ctx, extension:str):
+    async def reload(self, ctx, extension:str):
         '''Cog重新裝載'''
         self.bot.reload_extension(f"cmds.{extension}")
         print(f"[DCB] [{extension}] has reloaded.")
         await ctx.send(f"`{extension} 已重新載入。`")
 
+    @commands.command()
+    @commands.is_owner() 
+    async def load(self, ctx, extension:str):
+        '''Cog裝入'''
+        self.bot.load_extension(f"cmds.{extension}")
+        print(f"[DCB] [{extension}] has loaded.")
+        await ctx.send(f"`{extension} 已載入。`")
 
-    @commands.command() #關閉機器人
-    @commands.is_owner()     
+    @commands.command()
+    @commands.is_owner() 
+    async def unload(self, ctx, extension:str):
+        '''Cog移除'''
+        self.bot.unload_extension(f"cmds.{extension}")
+        print(f"[DCB] [{extension}] has unloaded.")
+        await ctx.send(f"`{extension} 已移除。`")
+
+    @commands.command() 
     async def bye(self,ctx):
         '''關閉機器人'''
         await ctx.send("`機器人已關閉。`")
